2890a03bde89197494e0bdbb1e1848c562592873
Chris Cormier <chris.hw.cormier@gmail.com>
2017-11-02 00:40:30 -0400

    Added analyze function to BasicSimulationList

--- roboscaffold_sim/simulators/basic_simulation_list.py
+++ roboscaffold_sim/simulators/basic_simulation_list.py
@@ -1,6 +1,6 @@
 import copy
 import traceback
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from roboscaffold_sim.coordinate import CoordinateList
 from roboscaffold_sim.simulators.basic_simulator import BasicSimulation
@@ -10,6 +10,9 @@ class BasicSimulationList:
     def __init__(self, initial_state: BasicSimulation = BasicSimulation()) -> None:
         self._working_state: BasicSimulation = initial_state
         self.states: List[BasicSimulation] = [copy.deepcopy(self._working_state)]
+        self._b_blocks = 0
+        self._s_blocks = 0
+        self._last_analyzed = 0
 
     @staticmethod
     def create_with_target_structure(target: CoordinateList):
@@ -39,6 +42,26 @@ class BasicSimulationList:
             if self._working_state.finished():
                 break
             else:
-                exception = self.update()
-                if exception:
+                if self.update():
                     break
+
+        self.analyze()
+        print(f'Analyzed the {self._last_analyzed} loaded states, currently '
+              f'{self._s_blocks} scaffolding has been used and {self._b_blocks} '
+              f'building blocks have beend used.')
+
+    def analyze(self) -> Tuple[int, int, int]:
+        if not self.states:
+            raise Exception("No simulation in SimulationList")
+        if len(self.states) > self._last_analyzed:
+            s_blocks = max(len(x.sim_state.s_blocks) for x in self.states[self._last_analyzed:])
+            b_blocks = max(len(x.sim_state.b_blocks) for x in self.states[self._last_analyzed:])
+            self._s_blocks = max(s_blocks, self._s_blocks)
+            self._b_blocks = max(b_blocks, self._b_blocks)
+
+            time = len(self.states)
+            self._last_analyzed = time
+
+        return self._s_blocks, self._b_blocks, self._last_analyzed
+
+
