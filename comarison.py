a3ad171e49de660052c6a110bb5a80841c19b032
Chris Cormier <ccormier@wpi.edu>
2017-11-02 17:05:29 -0400

    modified comparison to use ints to generate structs

--- comarison.py
+++ comarison.py
@@ -1,35 +1,63 @@
+from roboscaffold_sim.coordinate import Coordinate
 
-dimension = 9
+dimension = 5
 
 
-def furthest_true(target):
-    max_x = 0
-    max_y = 0
+def max_structs(dimension):
+    inner_dim = dimension - 1
+    outer_size = 2**(dimension**2+1)
+    inner_size = 2**(inner_dim**2)
+
+    max_s= outer_size - inner_size
+    return max_s
+
+
+max_s = max_structs(dimension)
+print(max_s)
+
+
+def create_struct(dimension, n):
+    struct = set()
+    add_inner_struct(dimension, n % (2 ** (dimension ** 2)), struct)
+    add_outer_struct(dimension, n // (2 ** (dimension ** 2)), struct)
+
+
+def add_inner_struct(dimension, n, struct):
     for y in range(dimension):
         for x in range(dimension):
-            if target[x][y]:
-                max_x = x
-                max_y = y
-    return max_x, max_y
+            if n % 2 != 0:
+                struct.add(Coordinate(x, 0))
+                n -= 1
+            n /= 2
+            if n == 0:
+                return
+    raise Exception('n should be 0 and function should have returned by now')
 
 
-def loop():
-    count = 0
-    target = [[False for _ in range(dimension)] for _ in range(dimension)]
+def add_outer_struct(dimension, n, struct):
+    for x in range(dimension-1):
+        if n % 2 != 0:
+            struct.add(Coordinate(x, 0))
+            n -= 1
+        n /= 2
+        if n == 0:
+            break
+    for y in range(dimension-1):
+        if n % 2 != 0:
+            struct.add(Coordinate(0, y))
+            n -= 1
+        n /= 2
+        if n == 0:
+            break
+    if n > 1:
+        raise Exception('n should be 0 or 1')
+    elif n == 1:
+        struct.add(Coordinate(0, 0))
 
-    while True:
-        x = 0
-        y = 0
-        while target[x][y]:
-            target[x][y] = False
-            x += 1
-            if x == dimension:
-                x = 0
-                y += 1
-                if y == dimension:
-                    return count
-        target[x][y] = True
-        count += 1
 
-
-print(loop())
+j = 0
+while j < max_s:
+    j += 1
+    create_struct(dimension, j)
+    if j % (int(max_s/100+.5)) == 0:
+        print(f'{int((j/max_s)*100)}% Done')
